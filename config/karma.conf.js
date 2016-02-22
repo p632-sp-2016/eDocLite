7681a3902ef71a667ee76f3547243ed7eae95bff
Jay Nagle <naglejay@gmail.com>
2016-02-21 19:23:15 -0500

    Corrected file loaders in karma js config file.

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -3,6 +3,7 @@
  */
 
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = function(config) {
     config.set({
@@ -13,20 +14,51 @@ module.exports = function(config) {
             'karma-mocha',
             'karma-webpack',
             'karma-phantomjs-launcher',
-            'karma-babel-preprocessor'
+            'karma-babel-preprocessor',
+            'karma-coverage',
+            'karma-sourcemap-loader',
+            'source-map'
         ],
         files: [
             'tests.webpack.js'
         ],
         preprocessors: {
-            'tests.webpack.js': ['webpack']
+            'tests.webpack.js': ['webpack', 'sourcemap']
 
         },
-        reporters: ['dots'],
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {
+            reporters: [
+                {
+                    type: 'text-summary'
+                },
+                {
+                    type: 'html',
+                    dir: path.join(__dirname, '../build/coverage')
+                },
+                {
+                    type: 'lcovonly',
+                    subdir: './../../'
+                }
+            ]
+        },
         webpack:{
+            devtool: 'inline-source-map',
             module: {
+                preLoaders: [
+                    { test: /\.js$/, loader: 'isparta', include: path.join(__dirname, '../src/'), exclude: [path.join(__dirname, '../test/')]}
+                ],
                 loaders: [
-                    { test: /\.js$/, loader: 'babel-loader' }
+                    { test: /\.js$/, loader: 'babel-loader' },
+                    {
+                        test: /\.less$/,
+                        exclude: /node_modules/,
+                        loader: 'style!css!less'
+                    },
+                    { test: /\.(woff|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/,  loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+                    { test: /\.ttf(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+                    { test: /\.eot(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+                    { test: /\.svg(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
                 ]
             }
         },
