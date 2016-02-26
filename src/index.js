486aff9cf77a7d9db55c0098b6c830fc1796012a
jaynagle <jaynagle@indiana.edu>
2016-02-26 16:56:12 -0500

    Implemented Todo app using Redux. Fixed #33
    Used online tutorial to implement basic Todo application using Redux and
    tested add item functionality.
    We can :
    - Create a new item
    - Toggle status
    - Change view filter

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 /**
  * Created by parikhv on 1/31/16.
  */
+import 'babel-polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Debug from 'debug';
 import Form from './components/Form';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
+
+let store = createStore(todoApp)
 var attachElement = document.getElementById('form');
+var todoElement = document.getElementById('todo');
 
 ReactDOM.render(<Form />, attachElement);
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>, todoElement);
