861367f8da5f0b7a2d7206932aa2b909e6216c4c
parikhv15 <vrajp12@gmail.com>
2016-03-02 13:07:09 -0500

    Fixed #46 and Worked on #48 -
    * Added redux-form so that form state is mainted by redux itself
    * Refactored some of the code

--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -1,63 +1,47 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import ReactDom from 'react-dom'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
 import {Button, Input,Glyphicon} from 'react-bootstrap';
+import { reduxForm } from 'redux-form';
 
 /**
  * this class facilitates creation of new todo items.
  */
 class AddTodo extends React.Component {
-    /**
-     * Class constructor
-     */
-    constructor(props) {
-        super(props);
-        /**
-         * @param input
-         */
-        this.state = {
-            input: {
-                value: ""
-            }
-        };
-    }
-
-    /**
-     * this function dispatches user input for creation of todo item.
-     */
-    todoSubmit = (event) => {
-        event.preventDefault();
-        if (!this.state.input.value.trim()) {
-            return;
-        }
-        this.props.dispatch(addTodo(this.state.input.value));
-        this.setState({input: {value: ""}})
-    };
-
-    /**
-     * this is the handleChange method for todo item
-     */
-    handleChange = (event) => {
-        this.setState({input: {value: event.target.value}});
-
-    };
 
     /**
      * this function renders input text for creation of new todo items.
      */
     render = () => {
+
+        const {fields: {text}, handleSubmit, dispatch, resetForm} = this.props;
         const submitTodoButton =  <Button bsStyle="primary" type="submit"><Glyphicon glyph="plus" /></Button>;
+
 
         return (
             <div>
-                <form onSubmit={this.todoSubmit.bind(this)}>
-                    <Input bsSize="large" type="text" placeholder="Todo item..." ref="input"  value={this.state.input.value} onChange={ this.handleChange.bind(this) } buttonAfter={submitTodoButton} />
+                <form onSubmit={handleSubmit(data => {
+                    dispatch(addTodo(data.text));
+                    resetForm();
+                })}>
+                    <Input bsSize="large" type="text" placeholder="Todo item..." {...text}  buttonAfter={submitTodoButton} />
                 </form>
             </div>
         )
     }
 }
+
+AddTodo.propTypes = {
+    fields: PropTypes.object.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+    resetForm: PropTypes.func.isRequired
+};
+
+AddTodo = reduxForm({
+    form: 'todo',
+    fields: ['text']
+})(AddTodo);
 
 AddTodo = connect()(AddTodo);
 
